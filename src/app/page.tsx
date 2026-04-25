5178cf46e767de86730dc886064ed6bcc99772c2
Abul Bashar <bashar35790@gmail.com>
2026-04-25 10:06:36 +0600

    setup font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-5xl text-orange-400 font-bold">Hello world</h1>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50  dark:bg-black">
+      <h1 className="text-5xl text-orange-400 font-bold ">Hello world</h1>
     </div>
   );
 }
